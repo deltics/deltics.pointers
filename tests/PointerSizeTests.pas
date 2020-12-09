82d9f1333dd82f788aeaaf7f879ca0331a4e93fd
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2020-12-09 20:17:24 +1300

    fix: Force compilation failure if 32/64BIT not $defined

--- tests/PointerSizeTests.pas
+++ tests/PointerSizeTests.pas
@@ -23,17 +23,27 @@ implementation
     Deltics.Pointers;
 
 
+  const
+    POINTER_SIZE = {$ifdef 64BIT} 8
+                   {$else}
+                     {$ifdef 32BIT} 4
+                     {$else}
+                       NEITHER_64BIT_NOR_32BIT_DEFINED
+                     {$endif}
+                   {$endif};
+
+
 { TPointerSizeTests }
 
   procedure TPointerSizeTests.IntPointerIsTheExpectedSizeForPlatform;
   begin
-    Test('sizeof(IntPointer)').Assert(sizeof(IntPointer)).Equals({$ifdef 64BIT} 8 {$else} 4 {$endif});
+    Test('sizeof(IntPointer)').Assert(sizeof(IntPointer)).Equals(POINTER_SIZE);
   end;
 
 
   procedure TPointerSizeTests.NativeUIntIsTheExpectedSizeForPlatform;
   begin
-    Test('sizeof(NativeUInt)').Assert(sizeof(NativeUInt)).Equals({$ifdef 64BIT} 8 {$else} 4 {$endif});
+    Test('sizeof(NativeUInt)').Assert(sizeof(NativeUInt)).Equals(POINTER_SIZE);
   end;
 
 
